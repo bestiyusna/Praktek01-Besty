28292134d22a801a4ea0d840700fe5a20769ab4e
ahmadkusairi <kusairi9616@gmail.com>
2018-03-26 20:33:09 +0800

    menambahkan dan memanggil method

--- src/praktek01/besty/mawaraksi.java
+++ src/praktek01/besty/mawaraksi.java
@@ -10,9 +10,36 @@ package praktek01.besty;
  * @author Mamed
  */
 public class mawaraksi {
+    public static void main(String[] args) {
+        
+    
     
     Bunga A = new Bunga();
     Bunga B = new Bunga();
-    Bunga C = new Bunga();                                                                               
+    Bunga C = new Bunga();  
     
+    A.warna ="Merah";
+    A.daun ="Keriting";
+    A.batang ="Berduri";
+    A.aroma ="Segar";
+    A.jenis ="Mister_Lincoln";
+    
+    B.warna ="putih";
+    B.daun ="Keriting";
+    B.batang ="Berduri";
+    B.aroma ="Segar";
+    B.jenis ="Mawar iceberg";
+    
+    C.warna ="kuning";
+    C.daun ="Keriting";
+    C.batang ="Berduri";
+    C.aroma ="Segar";
+    C.jenis ="Graham_thomas";
+    
+        A.cetakInfo();
+        System.out.println();
+        B.cetakInfo();
+        System.out.println();
+        C.cetakInfo();
+}
 }
